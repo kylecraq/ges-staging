8be1f018c528ec764be98985313ffe5d03294ec0
kylecraq <kyle@craq.it>
2025-11-14 22:11:32 +0100

    feature(typography): text hero

--- src/components/section/hero.tsx
+++ src/components/section/hero.tsx
@@ -1,9 +1,18 @@
+'use client';
 import Image from 'next/image';
+import { Heading } from '@/components/typography';
+import { useTranslations } from 'next-intl';
 
 export const Hero = () => {
+  const t = useTranslations('HomePage');
   return (
     <header className="min-h-screen">
       <div className="relative aspect-[460/327]">
+        <Heading as="h1" size="xxl">
+          {t.rich('title', {
+            primary: (chunks) => <span className="text-primary">{chunks}</span>,
+          })}
+        </Heading>
         <Image
           src="/hero/next-charge-hero.png"
           alt="Next charge hero"
