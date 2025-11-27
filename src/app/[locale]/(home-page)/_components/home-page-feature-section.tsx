2b0d6d247cd6dd0da7b1a016f26e1b9004693db4
kylecraq <kyle@craq.it>
2025-11-27 09:52:14 +0100

    feature iphone section

--- src/app/[locale]/(home-page)/_components/home-page-feature-section.tsx
+++ src/app/[locale]/(home-page)/_components/home-page-feature-section.tsx
@@ -1,29 +1,37 @@
 'use client';
 
 import { IntroParagraph } from '@/components/nextcharge-ui/sections/intro-paragraph';
-import { Chip, FeatureListWithImage } from '@/components/nextcharge-ui/sections/feature-list-with-image';
+import {
+  Chip,
+  FeatureListWithImage,
+} from '@/components/nextcharge-ui/sections/feature-list-with-image';
 import { useTranslations } from 'next-intl';
 
 export const HomePageFeatureSection = () => {
-  const t = useTranslations("HomePage.SectionFeature")
+  const t = useTranslations('HomePage.SectionFeature');
 
   const chips: Chip[] = [
     {
       badge: 'Smartphone',
-      description: 'Connesso sempre al tuo smartphone, ricarica in modo semplice e consulta le tue transazioni. Connesso sempre al tuo smartphone, ricarica in modo semplice e consulta le tue transazioni.',
+      description:
+        'Connesso sempre al tuo smartphone, ricarica in modo semplice e consulta le tue transazioni. Connesso sempre al tuo smartphone, ricarica in modo semplice e consulta le tue transazioni.',
     },
     {
       badge: 'Smartcard Rfid',
-      description: 'La carta Rfid NextCharge è sempre connessa al tuo wallet e ti permette di ricaricare anche senza smartphone.',
+      description:
+        'La carta Rfid NextCharge è sempre connessa al tuo wallet e ti permette di ricaricare anche senza smartphone.',
     },
     {
       badge: 'Plug&charge',
-      description: 'La carta Rfid NextCharge è sempre connessa al tuo wallet e ti permette di ricaricare anche senza smartphone.',
+      description:
+        'La carta Rfid NextCharge è sempre connessa al tuo wallet e ti permette di ricaricare anche senza smartphone.',
     },
   ];
 
-  return <article className="flex flex-col">
-    <IntroParagraph title={"title"} kicker={"kicker"}/>
-    <FeatureListWithImage chips={chips} />
-  </article>;
+  return (
+    <article className="flex flex-col">
+      <IntroParagraph title={t('title')} kicker={t('kicker')} />
+      <FeatureListWithImage chips={chips} imgAlt="Iphone" imgMbl="/hero/feature-phone.png" imgDsk="/hero/feature-phone.png" />
+    </article>
+  );
 };
--- src/components/nextcharge-ui/sections/feature-list-with-image.tsx
+++ src/components/nextcharge-ui/sections/feature-list-with-image.tsx
@@ -4,6 +4,7 @@ type FeatureListWithImageProps = {
   chips: Chip[];
   imgDsk?: string;
   imgMbl?: string;
+  imgAlt?: string;
 };
 
 export type Chip = {
@@ -12,25 +13,39 @@ export type Chip = {
 };
 
 export const FeatureListWithImage = (props: FeatureListWithImageProps) => {
-  const { chips, imgDsk, imgMbl } = props;
+  const { chips, imgDsk, imgMbl, imgAlt } = props;
 
   return (
-    <section className="grid grid-cols-2">
-      <div className="flex flex-col gap-10 w-8/12">
+    <section className="grid md:grid-cols-2 gap-10">
+      <div className="flex md:w-8/12 flex-col gap-10">
         {chips.map((chip, index) => {
           return (
-            <div className="flex flex-col items-start gap-4">
-              <Kicker text={chip.badge} variant="full" key={index} />
-              <p className="p-8 rounded-4xl bg-neutral-100">{chip.description}</p>
+            <div key={index} className="flex flex-col items-start gap-4">
+              <Kicker text={chip.badge} variant="full" />
+              <p className="rounded-4xl bg-neutral-100 p-8">
+                {chip.description}
+              </p>
             </div>
           );
         })}
       </div>
-      {/*<img*/}
-      {/*  src={imgDsk}*/}
-      {/*  alt={'Ciao'}*/}
-      {/*  className="aspect-4/4 object-cover object-center"*/}
-      {/*/>*/}
+      <div className="grid place-items-center order-first md:order-last">
+        {imgDsk ? (
+          <img
+            src={imgDsk}
+            alt={imgAlt || 'Image'}
+            className="hidden aspect-4/4 h-full w-full object-contain object-center md:block"
+          />
+        ) : null}
+
+        {imgDsk ? (
+          <img
+            src={imgMbl}
+            alt={imgAlt || 'Image'}
+            className="aspect-4/4 h-full w-full object-contain object-center md:hidden"
+          />
+        ) : null}
+      </div>
     </section>
   );
 };
