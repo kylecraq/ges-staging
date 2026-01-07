1831d6f6f3c54a546fc656c2bcf5b745fc1aa5c6
kylecraq <kyle@craq.it>
2026-01-07 09:31:36 +0100

    slider colors

--- src/components/ges-ui/sections/fancy-carousel.tsx
+++ src/components/ges-ui/sections/fancy-carousel.tsx
@@ -38,10 +38,10 @@ export function FancyCarousel(props: FancyCarouselProps) {
     type: 'bullets',
     clickable: true,
     bulletActiveClass:
-      'text-neutral-100 outline-2 outline-offset-3 outline-primary-variant',
+      'text-neutral-900 outline-2 outline-offset-3 outline-primary-variant',
     renderBullet: (index, className) => {
       const iconHtml = renderToStaticMarkup(slides[index].icon);
-      return `<span class='${className} transition-all duration-200 ease-slow bg-neutral-0 min-w-4 cursor-pointer p-2 rounded-full [&_svg]:size-5'>${iconHtml}</span>`;
+      return `<span class='${className} text-neutral-400 transition-all duration-200 ease-slow bg-neutral-0 min-w-4 cursor-pointer p-2 rounded-full [&_svg]:size-5'>${iconHtml}</span>`;
     },
   };
 
