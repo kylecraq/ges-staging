1d46fff3951a1a9eda8c7418c482bdb7383515a9
kylecraq <kyle@craq.it>
2026-02-02 12:51:09 +0100

    open graph

--- src/app/[locale]/(home-page)/page.tsx
+++ src/app/[locale]/(home-page)/page.tsx
@@ -22,6 +22,11 @@ export async function generateMetadata({
 
   return {
     title: t('title'),
+    description: t("description"),
+    openGraph: {
+      title: t('title'),
+      description: t("description")
+    },
   };
 }
 
